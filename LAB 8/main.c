523e861a8e875912c1757aa609e725bbbb4effab
dmosher42@gmail.com <dmosher42@gmail.com>
2014-04-10 17:38:19 -1000

    Initial Commit

---
+++ LAB 8/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LAB 8
+//
+//  Created by Dee on 4/10/14.
+//  Copyright (c) 2014 Dee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
