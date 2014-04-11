b603904ee8c7efb67a82bb2d0422a09c09753e75
kensw <kensw@hawaii.edu>
2014-04-10 17:51:42 -1000

    Second commit from local git

---
+++ Lab8/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Lab8
+//
+//  Created by Ken Wallace on 4/10/14.
+//  Copyright (c) 2014 Ken Wallace. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
